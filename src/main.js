8e52bec1e6a643693760d2a6fbc9ea811e308c02
EmilianoRivera <jriveras2100@alumno.ipn.mx>
2024-03-20 01:12:12 -0600

    FUNCIONA EL EL CAMBIO DE DATOS, PERO NO DE REGISTRO DE NUEVAS CUENTAS

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import {
   conserverStrBool,
   obtenerUsuarios,
   obtenerEstadoCuenta,
-  obtenerCuentas
+  obtenerCuentas,
 } from "../lib/conversor.js";
 
 const usuariosCollection = collection(db, "usuarios");
@@ -22,7 +22,6 @@ const unListenUser = onSnapshot(
   usuariosCollection,
   (snapshot) => {
     snapshot.docChanges().forEach(async (change) => {
-
       if (change.type === "added" || change.type === "modified") {
         if (change.doc.data().estadoCuenta === true) {
           const usuarioDataModificado = change.doc.data();
@@ -32,20 +31,19 @@ const unListenUser = onSnapshot(
         }
       }
       if (change.doc.data().estadoCuenta === false) {
-        console.log("FALSOOO")
-        const newFecha = conversorFechaToISO(change.doc.data().fechaNacimiento)
-        const objCuentas =  await obtenerEstadoCuenta(
+        console.log("FALSOOO");
+        const newFecha = conversorFechaToISO(change.doc.data().fechaNacimiento);
+        const objCuentas = await obtenerEstadoCuenta(
           change.doc.data().estadoCuenta,
           change.doc.data().uid,
-         newFecha,
-         change.doc.data().correo
-
+          newFecha,
+          change.doc.data().correo
         );
         console.log(objCuentas);
         console.log("desactivado");
       }
 
-       if (change.type === "removed") {
+      if (change.type === "removed") {
         //logica para remover al usuario de postgre
         console.log("Usuario eliminado:", change.doc.data());
       }
@@ -57,41 +55,60 @@ const unListenUser = onSnapshot(
 );
 
 async function ModificarUsuario(usuarioDataModificado) {
-  const { nombre, apellidoPaterno, apellidoMaterno, uid, correo, estadoCuenta, fechaNacimiento } =
-    usuarioDataModificado;
-  const objUsuarios = await obtenerUsuarios();
-console.log(estadoCuenta)
-  const newFecha = conversorFechaToISO(fechaNacimiento)
-  for (const usuario of objUsuarios)  {
-    if (usuario.nomu_fb_uid === uid) {
+  const {
+    nombre,
+    apellidoPaterno,
+    apellidoMaterno,
+    uid,
+    correo,
+    estadoCuenta,
+    fechaNacimiento,
+  } = usuarioDataModificado;
+  const newFecha = conversorFechaToISO(fechaNacimiento);
+
+  const objUsuarios = await obtenerUsuarios(); //regresa Nombre_Usuario
+  const objCuentas = await obtenerCuentas(); //regresa Usuario
+  console.log(estadoCuenta);
+  console.log(uid);
+
+  for (const nomUsuarios of objUsuarios) {
+    console.log(typeof uid);
+    if (nomUsuarios.nomu_fb_uid === uid) {
+      //este for lo hago para poder extraer el usu_id de usuario
+      //la forma de ir modificando los datos es de ir de nombre usuario
+      console.log("ENTRE AL IF")
       try {
-        const actualizarCuenta = await prisma.usuario.update({
+        const actualizarCuenta = await prisma.nombre_Usuario.update({
           where: {
-            usu_id: usuario.usu_id,
+            nomu_id: nomUsuarios.nomu_id,
           },
           data: {
-            usu_fechanac: newFecha,
-            usu_correo: correo,
-            usu_estado_cuenta:  estadoCuenta,
-           
-            Nombre_Usuario: {
-                create: {
-                    nomu_nombre: nombre,
-                    nomu_appat: apellidoPaterno,
-                    nomu_apmat: apellidoMaterno,
-                                  
-                }
-            }
+            nomu_nombre: nombre,
+            nomu_appat: apellidoPaterno,
+            nomu_apmat: apellidoMaterno,
+            nomu_fb_uid: uid,
+
+            Usuario: {
+              update: {
+                usu_fechanac: newFecha,
+                usu_correo: correo,
+                usu_estado_cuenta: estadoCuenta,
+              },
+            },
           },
-          
+          include: {
+            Usuario: true,
+          },
         });
 
         console.log("Se actualizo con exito", actualizarCuenta);
       } catch (error) {
         console.error("Error al actualizar datos del usuario:", error);
       }
-      
-      console.log("Actualizando...."); //actualizar datos
     }
   }
 }
+
+/*
+ 
+ */
